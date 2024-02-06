ab95996555ac2ef60baefa0f1b9255a02ce25bc7
JM-Rib <jeanmarin.ribaric@gmail.com>
2024-02-06 17:50:06 +0100

    Ajout d'argument pour position des modèles

--- src/Model.tsx
+++ src/Model.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const Model: React.FC<{ modelPath: string }> = ({ modelPath }) => {
+const Model: React.FC<{ modelPath: string, x?: Int16Array, y?: Int16Array, z?: Int16Array}> = ({ modelPath, x = 0, y = 0, z = 0 }) => {
   const gltf = useLoader(GLTFLoader, modelPath);
-  return <primitive object={gltf.scene} />;
+  return <primitive object={gltf.scene} position={[x, y, z]}  />;
 };
 
 export default Model;
