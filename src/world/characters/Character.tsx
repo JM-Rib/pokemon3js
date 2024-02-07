b649bf5fa5c7790aa0596075af65aaf329044006
BastienTLC <bastientalec@orange.fr>
2024-02-07 09:50:12 +0100

    fix : récupération de l'état de l'element 3d enfant dans le parent

--- src/world/characters/Character.tsx
+++ src/world/characters/Character.tsx
@@ -1,19 +1,25 @@
 import ModelLoader from "../../utils/ModelLoader.tsx";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useBox } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
 
 const Character = () => {
     const [ref] = useBox(() => ({ mass: 1, position: [0, 0, 0] }));
+    const CharacterRef = useRef();
 
-    useFrame(({ clock }) => {
-        ref.current.rotation.y = clock.getElapsedTime();
-        console.log(ref);
+    useFrame(() => {
+        if (CharacterRef.current)
+            CharacterRef.current.rotation.y += 0.01;
     });
+
+    useEffect(() => {
+        console.log(CharacterRef.current);
+    }, [CharacterRef.current]);
 
     return (
         <>
-            <ModelLoader modelPath="characters.glb" ref={ref}/>
+            <ModelLoader modelPath="characters.glb" ref={ref} meshRef={CharacterRef} />
+            {/* Utilisez meshRef.current ici */}
         </>
     );
 }
