8619137424f63d79b7266f8982d21e457ad0b61f
BastienTLC <bastientalec@orange.fr>
2024-02-07 18:01:29 +0100

    chore : suppression temporaire le map remplacement pas une simple surface

--- src/world/characters/Character.tsx
+++ src/world/characters/Character.tsx
@@ -7,24 +7,17 @@ const Character = () => {
     const [ref] = useBox(() => ({ 
         mass: 1, 
         collisionFilterGroup: 1, 
-        position: [2.5, 0, 2.5], //Permet que le perso tombe droit
+        position: [0, 0, 0], //Permet que le perso tombe droit
         rotation: [0, 0, 0], 
     }));
-    const CharacterRef = useRef();
 
-    // useFrame(() => {
-    //     if (CharacterRef.current)
-    //         CharacterRef.current.rotation.y += 0.01;
-    //         CharacterRef.current.position.y = Math.sin(0.01 * Date.now());
-    // });
+    useFrame(() => {
+    });
 
-    useEffect(() => {
-        console.log(CharacterRef.current);
-    }, [CharacterRef.current]);
 
     return (
         <>
-            <ModelLoader modelPath="characters.glb" ref={ref} meshRef={CharacterRef} physicsProps={{ ref }} />
+            <ModelLoader modelPath="characters.glb" ref={ref} physicsProps={{ ref }} />
             {/* Utilisez meshRef.current ici */}
         </>
     );
--- src/world/pokemons/Pokemons.tsx
+++ src/world/pokemons/Pokemons.tsx
@@ -1,13 +1,14 @@
 import ModelLoader from "../../utils/ModelLoader.tsx";
-import React from "react";
+import React, {useRef} from "react";
 import {useBox} from "@react-three/cannon";
 
 const Pokemons = () => {
     const [ref] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+    const PokemonRef = useRef();
 
     return (
         <>
-            <ModelLoader modelPath="monster.glb" ref={ref}/>
+            <ModelLoader modelPath="monster.glb" ref={ref} />
         </>
     );
 }
