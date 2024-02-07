501e7716f8e1be1a8310f55287f9ccd1c7995bea
BastienTLC <bastientalec@orange.fr>
2024-02-07 19:44:54 +0100

    feat : deplacement perso

--- src/world/characters/Character.tsx
+++ src/world/characters/Character.tsx
@@ -2,30 +2,25 @@ import ModelLoader from "../../utils/ModelLoader.tsx";
 import React, { useEffect, useRef } from "react";
 import { useBox } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
+import Controls from "./Controls.tsx";
 
 const Character = () => {
-    const [ref] = useBox(() => ({ 
-        mass: 1, 
-        collisionFilterGroup: 1, 
-        position: [2.5, 0, 2.5], //Permet que le perso tombe droit
-        rotation: [0, 0, 0], 
+    const [ref, api] = useBox(() => ({
+        mass: 1,
+        collisionFilterGroup: 1,
+        position: [0, 0, 0], // Permet que le perso tombe droit
+        rotation: [0, 0, 0],
     }));
-    const CharacterRef = useRef();
 
-    // useFrame(() => {
-    //     if (CharacterRef.current)
-    //         CharacterRef.current.rotation.y += 0.01;
-    //         CharacterRef.current.position.y = Math.sin(0.01 * Date.now());
-    // });
-
-    useEffect(() => {
-        console.log(CharacterRef.current);
-    }, [CharacterRef.current]);
+    useFrame((state, delta) => {
+        // Faites tourner la boîte de collision ici
+        api.rotation.set(0, Math.sin(state.clock.elapsedTime) * 0.5, 0);
+    });
 
     return (
         <>
-            <ModelLoader modelPath="characters.glb" ref={ref} meshRef={CharacterRef} physicsProps={{ ref }} />
-            {/* Utilisez meshRef.current ici */}
+            <ModelLoader modelPath="characters.glb" ref={ref} physicsProps={{ ref }} />
+            <Controls characterApi={api} />
         </>
     );
 }
