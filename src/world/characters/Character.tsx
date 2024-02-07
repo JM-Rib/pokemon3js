aacabff6d93a02b048e70b7718869263f0ed4d47
JM-Rib <jeanmarin.ribaric@gmail.com>
2024-02-07 19:23:12 +0100

    Ajout camera personnage

--- src/world/characters/Character.tsx
+++ src/world/characters/Character.tsx
@@ -1,7 +1,8 @@
 import ModelLoader from "../../utils/ModelLoader.tsx";
 import React, { useEffect, useRef } from "react";
 import { useBox } from "@react-three/cannon";
-import { useFrame } from "@react-three/fiber";
+import { useFrame,  useThree } from "@react-three/fiber";
+import useFollowCam from "../../utils/useFollowCam.tsx"; // Import the useFollowCam hook
 
 const Character = () => {
     const [ref] = useBox(() => ({ 
@@ -11,9 +12,12 @@ const Character = () => {
         rotation: [0, 0, 0], 
     }));
 
-    useFrame(() => {
-    });
+    const { camera } = useThree(); // Get the camera from useThree hook
+    const { pivot, alt, yaw, pitch } = useFollowCam(ref, [0, 1, 1.5]); // Call the useFollowCam hook
 
+    useFrame(() => {
+        // You can add custom logic related to character movement or animation here
+    });
 
     return (
         <>
