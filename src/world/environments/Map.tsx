238c60dffc8da0709a87fbf4938a632d6a484cde
JM-Rib <jeanmarin.ribaric@gmail.com>
2024-02-10 19:08:17 +0100

    Creation branche pour collision sur la map

--- src/world/environments/Map.tsx
+++ src/world/environments/Map.tsx
@@ -1,19 +1,22 @@
 import ModelLoader from "../../utils/ModelLoader.tsx";
-import React, { useEffect, useRef } from "react";
-import {usePlane} from "@react-three/cannon";
+import React, { useEffect, useRef, useMemo } from "react";
+import {usePlane, useTrimesh} from "@react-three/cannon";
 import {Plane} from "@react-three/drei";
-import {DoubleSide} from "three";
+import {TorusGeometry} from "three";
 
 const Map = () => {
-    const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], material: 'ground' }), useRef())
+    const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], material: 'ground' }), useRef());
+    const geometry = useMemo(() => new TorusGeometry(0.75, 0,1), [[0.75, 0,1]]);
+    const [isleRef] = useTrimesh(() => ({
+      args: [geometry.attributes.position.array, geometry.index?.array],
+      material: 'ring',
+      position: [0, 1, 0],
+    }));
 
 
     return (
         <>
-            {/*<ModelLoader modelPath="island.glb" ref={ref}  physicsProps={{ ref }} />*/}
-            <Plane args={[100, 100]} ref={ref} rotation={[0, 0, 0]} position={[0, 0, 0]} receiveShadow>
-                <meshPhysicalMaterial attach="material" color="green" side={ DoubleSide} />
-            </Plane>
+            <ModelLoader modelPath="island.glb" ref={isleRef}  physicsProps={{ isleRef }} />
         </>
     );
 }
